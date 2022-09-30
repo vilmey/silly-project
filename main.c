75518f5be061414740c5754eac17fffa81cb475c
vilmeyr@gmail.com <vilmey.filho@eldorado.org.br>
2022-09-30 18:39:22 -0300

    Adding git-modules messages

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "git-modules/messages/prettyMessage.h"
 
 
 int main (int argc, char **argv)
 {
-    printf("Hello World\n");
+    
+    printf("%s",message(2));
 
     return 0;
 }
